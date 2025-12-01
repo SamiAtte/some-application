c6941f6e00b9ca9451cca0ee40231636e0fba23e
SamiAtte <51129384+SamiAtte@users.noreply.github.com>
2025-12-01 20:11:06 +0200

    Added pool- and post-viewing pages.

--- models/pages.py
+++ models/pages.py
@@ -96,20 +96,30 @@ def post_pool(post_pool_id):
   posts_query = db.query(posts_sql, [post_pool_id])
   posts = multi_list(posts_query, list(range(0,6)))
 
-  return render_template("post-pool.html", post_pool_info=pool_info[0], posts=posts)
+  return render_template("pool.html", post_pool_info=pool_info[0], posts=posts)
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
   post_sql = '''
-    SELECT A.post_headline, A.poster, B.content, C.post_pool_title, D.username
+    SELECT A.post_headline, A.poster, B.content, C.post_pool_title, D.username, F.count
     FROM Posts A
     LEFT JOIN PostContents B ON A.id = B.post
     LEFT JOIN PostPools C on C.id = A.post_pool
     LEFT JOIN Users D on D.id = A.poster
+    LEFT JOIN CommentsSections E ON A.id = E.post
+    LEFT JOIN (SELECT comments_section, COUNT(*) AS count FROM Messages GROUP BY comments_section) F
+      ON F.comments_section = E.id
     WHERE A.id = ?
   '''
   post_query = db.query(post_sql, [post_id])
-  post = multi_list(post_query,list(range(0,5)))
+  post = multi_list(post_query,list(range(0,6)))
 
   return render_template("post.html", post=post[0])
+
+
+
+
+
+
+
 
